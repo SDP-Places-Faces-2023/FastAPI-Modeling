017f3331029b5c2514947f1f890a6db07d9e7376
sadig05 <Sadiq.letifli70@gmail.com>
2023-02-05 17:50:45 +0400

    feat: middleware added 3

--- server.py
+++ server.py
@@ -3,13 +3,20 @@ import json
 
 import uvicorn
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import yolov5s
 from model import predict, read_imagefile
 
 mserver = FastAPI()
 
-
+mserver.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @mserver.post("/predict/image")
 async def predict_api(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
